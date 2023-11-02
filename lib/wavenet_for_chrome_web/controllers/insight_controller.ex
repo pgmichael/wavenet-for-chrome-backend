0bf9a5fc679eaf607fe56a7381bae8b42623d6c3
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2023-11-02 11:36:01 -0400

    Removed unused `create` in `InsightController`
    Since both paid and free requests are now handled in TextToSpeechController we don't need a separate endpoint for creation

--- lib/wavenet_for_chrome_web/controllers/insight_controller.ex
+++ lib/wavenet_for_chrome_web/controllers/insight_controller.ex
@@ -17,28 +17,4 @@ defmodule WavenetForChromeWeb.InsightController do
 
     ok(conn, insights)
   end
-
-  def create(conn, params) do
-    ip_address = conn.remote_ip |> Tuple.to_list() |> Enum.join(".")
-    user = get_user_by_secret_key(conn, return_nil: true)
-
-    Map.merge(params, %{
-      user_id: user |> get_in([:id]),
-      ip_address: ip_address
-    })
-
-    changeset = Insight.changeset(%Insight{}, params)
-
-    case Repo.insert(changeset) do
-      {:ok, insight} ->
-        conn
-        |> put_status(:created)
-        |> json(%{data: insight})
-
-      {:error, changeset} ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> json(%{errors: changeset})
-    end
-  end
 end
